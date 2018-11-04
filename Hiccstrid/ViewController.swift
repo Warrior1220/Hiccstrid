31f91847eb2bf61dc43354285b91ffb31bbe13e6
张笑睿 <1622360009@qq.com>
2018-11-04 08:01:23 +0800

    Initial Commit

---
+++ Hiccstrid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hiccstrid
+//
+//  Created by Warrior on 2018/11/4.
+//  Copyright © 2018 Warrior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
